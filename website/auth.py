904be0ef6eaee5be2d5b169f5496c986c6f02d6f
Said Smajić <83903364+said-smajic@users.noreply.github.com>
2023-02-01 11:37:05 +0100

    sign up database done

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -23,11 +26,15 @@ def sign_up():
         elif len(firstName) < 2:
             flash ('First Name must be greater than 2 characters.', category='error')
         elif password1 != password2:
-            pflash ('Passwords don\'t match.', category='error')
+            flash ('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash ('Email must be greater than 7 characters.', category='error')
         else:
-            # add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash ('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
